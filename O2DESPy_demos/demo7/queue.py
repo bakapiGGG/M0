4ffd3ed493f11543c21be1e24c637346b7cc2560
Kang Heng <limkangheng@u.nus.edu>
2020-12-23 12:34:15 +0800

    demo7

---
+++ O2DESPy_demos/demo7/queue.py
@@ -0,0 +1,31 @@
+from O2DESPy.sandbox import Sandbox
+from O2DESPy.log.logger import Logger
+
+
+class Queue(Sandbox):
+    def __init__(self, seed=0):
+        super().__init__()
+        self.__seed = seed
+        self.__number_waiting = 0
+    
+    @seed.setter
+    def seed(self, value):
+        self.__seed = value
+
+    @property
+    def on_generate(self):
+        return self.__on_generate
+
+    @property
+    def number_waiting(self):
+        return self.__number_waiting
+
+    def enqueue(self):
+        self.__number_waiting += 1
+        print("{0}\t{1}\tEnqueue. #Waiting: {2}".format(self.clock_time, type(self).__name__, self.__number_waiting))
+        Logger.info("{0}\t{1}\tEnqueue. #Waiting: {2}".format(self.clock_time, type(self).__name__, self.__number_waiting))
+
+    def dequeue(self):
+        self.__number_waiting -= 1
+        print("{0}\t{1}\tDequeue. #Waiting: {2}".format(self.clock_time, type(self).__name__, self.__number_waiting))
+        Logger.info("{0}\t{1}\tDequeue. #Waiting: {2}".format(self.clock_time, type(self).__name__, self.__number_waiting))
